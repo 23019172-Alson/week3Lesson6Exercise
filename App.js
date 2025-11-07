d64d2ab3e3a2affb0d8d519ca71d856466f1e196
23019172-Alson <23019172@myrp.edu.sg>
2025-11-07 22:04:42 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, Image, Text, View, TextInput, Button, Alert, TouchableOpacity,ToastAndroid } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import React, {useState} from 'react';
+
+
+const App =() => {
+  const [q1, setQ1] = useState('');
+  const [q2, setQ2] = useState('');
+  const [q3, setQ3] = useState('');
+
+  const handleSubmit = () => {
+    
+    let score = 0;
+    if (q1 === 'lambo') score++;
+    if (q2 === 'maserati') score++;
+    if (q3 === 'bugatti') score++;
+
+    Alert.alert('Result', `You have ${score} correct answers!`);
+  };
+  return (
+
+    <View>
+      <Text>Car Quiz</Text>
+
+      
+      <ScrollView>
+      <Text>What car is this?</Text>
+      <Image style={{width: 400, height: 500}} source={require('./img/lambo.jpg')} />
+      <Picker selectedValue={q1} onValueChange={(value) => setQ1(value)}>
+        <Picker.Item label="Select an item..." value="" color="gray" />
+        <Picker.Item label="lambo"value="lambo"></Picker.Item>
+        <Picker.Item label ="maserati" value="maserati"></Picker.Item>
+        <Picker.Item label ="bugatti" value="bugatti"></Picker.Item>
+      </Picker>
+
+      <Text>What car is this?</Text>
+      <Image style={{width: 400, height: 500}} source={require('./img/maserati.jpg')} />
+      <Picker selectedValue={q2} onValueChange={(value) => setQ2(value)}>
+        <Picker.Item label="Select an item..." value="" color="gray" />
+        <Picker.Item label="lambo"value="lambo"></Picker.Item>
+        <Picker.Item label ="maserati" value="maserati"></Picker.Item>
+        <Picker.Item label ="bugatti" value="bugatti"></Picker.Item>
+      </Picker>
+
+      <Text>What car is this?</Text>
+      <Image style={{width: 500, height: 500}} source={require('./img/bugatti.jpg')} />
+      <Picker selectedValue={q3} onValueChange={(value) => setQ3(value)}>
+        <Picker.Item label="lambo"value="lambo"></Picker.Item>
+        <Picker.Item label ="maserati" value="maserati"></Picker.Item>
+        <Picker.Item label ="bugatti" value="bugatti"></Picker.Item>
+      </Picker>
+
+       <Button title="Submit Answers" onPress={handleSubmit} />
+      <StatusBar translucent={false} />
+      
+
+
+      
+      </ScrollView>
+    </View>
+    
+  )
+}  
+
+
+
+export default App;
+
